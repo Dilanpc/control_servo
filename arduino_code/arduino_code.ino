71e9fc7c55af68286090e013c1abc33227a8fd6e
Dilan <diporrasc@unal.edu.co>
2024-05-22 14:40:02 -0500

    adding arduino code

---
+++ arduino_code/arduino_code.ino
@@ -0,0 +1,38 @@
+#include <Servo.h>
+
+int angle=0;
+Servo servo;
+const int pinServo = 8;
+
+void setup(){
+  Serial.begin(9600);
+  servo.attach(pinServo);
+
+}
+
+void loop(){
+  // read entrance
+  if (Serial.available() >= 4){
+    if (Serial.peek() != '\n'){
+      // se reciben número en ASCII, por ello se resta el caracter '0' en cada dígito
+      angle = (Serial.read() - '0')*100 + (Serial.read() - '0')*10 + Serial.read() - '0';
+      Serial.println(angle);
+    }
+    printAll();
+  }
+
+  servo.write(angle);
+
+  delay(100);
+
+}
+
+
+void clear(){
+  while(Serial.available() > 0) Serial.read(); //Descartar excedentes
+}
+void printAll(){
+  while (Serial.available() > 0){
+    Serial.print(Serial.read());
+  }
+}
